dcefcefc0fa2f247afdfa1090c323d01e1108de5
𝐱𝐂𝐇𝐀𝐌𝐎 🧡⚡ <herokubot458@gmail.com>
2025-02-04 12:20:58 +0530

    Update session.js

--- session.js
+++ session.js
@@ -40,6 +40,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your session id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~uywzRAgK#2JKzOSNA1qGJrTy0Sq27dUiRwLYQDHPARV2mGMh9MT4",
 PORT: process.env.PORT || "8000"
 };
